b2f123bc404d6bd46aae7ed16fe10088f13232ff
ir0nsand <ninesowngoal@protonmail.com>
2025-05-26 19:28:07 -0400

    Folder Removal
    Removed folders that were no longer needed to streamline repository

--- Old-backup/html/js/expanders.js
+++
@@ -1,9 +0,0 @@
-(() => {
-  const expanders = document.querySelectorAll('.expander');
-
-  for (const expander of expanders) {
-    expander.addEventListener('click', () => {
-      expander.dataset.expanded = !JSON.parse(expander.dataset.expanded);
-    })
-  }
-})()
