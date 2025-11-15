98d621426c832515d6fbff15ce7ac30bec2f807e
ir0nsand <ninesowngoal@protonmail.com>
2025-11-15 14:18:04 -0500

    rpg
    david update for puzzle

---
+++ leaderboard.js
@@ -0,0 +1,16 @@
+leaderboard = {
+	"Prize winners": {
+		"Owl treasure hunt": ["Roshan", "Sr", "Anonymous"],
+		"Top labyrinth": ["Roshan"],
+		"Shuffle maze": ["Sr", "Lb"],
+		"Owl assembly": ["Lb"],
+		"RPG": []
+	},
+	"Easter eggs": {
+		"Clover": [],
+		"Jester": [],
+		"Cup": [],
+		"unknown": [],
+		"Lions key": []
+	}
+}
